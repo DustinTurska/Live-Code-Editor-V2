e5a14fc0ed99fcef6bed688edcee85944da8ef44
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-07-17 14:23:43 -0500

    Update UseSwitchChain.tsx

--- live-code/snippets/network/UseSwitchChain.tsx
+++ live-code/snippets/network/UseSwitchChain.tsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { ThirdwebProvider, useSwitchChain } from "@thirdweb-dev/react";
+import React, { useState } from "react";
+import { ThirdwebProvider, useSwitchChain, ConnectWallet } from "@thirdweb-dev/react";
 import { Goerli } from "@thirdweb-dev/chains";
 import LiveCodeEditor from "../../LiveCodeEditor";
 
-const scope = { React, Goerli, ThirdwebProvider, useSwitchChain };
+const scope = { React, Goerli, ThirdwebProvider, useSwitchChain, ConnectWallet, useState };
 
 const code = `function App() {
-    const switchChain = useSwitchChain();
-    return (
-      <button onClick={() => switchChain(Goerli.chainId)}>
-        Switch to Goerli
-      </button>
-    );
-  }`;
+  const switchChain = useSwitchChain();
+  const [isConnected, setConnected] = useState(false);
+
+  const handleSwitchChain = async () => {
+    await switchChain(Goerli.chainId);
+    setConnected(true);
+  };
+
+  return (
+    <div>
+      {!isConnected && (
+        <button onClick={handleSwitchChain}>
+          Switch to Goerli
+        </button>
+      )}
+
+      {isConnected && <ConnectWallet />}
+    </div>
+  );
+}`;
 
 export default function UseSwitchChain() {
   return <LiveCodeEditor code={code} additionalScope={scope} />;
