c4e6dc57cd86a938e6ce216c39da1ca91ca73712
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-07-17 16:26:45 -0500

    Update index.tsx

--- pages/nfts/index.tsx
+++ pages/nfts/index.tsx
@@ -14,29 +14,47 @@ const NFTs: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <a href="https://portal.thirdweb.com/react/react.usenft" target="_blank">
         <h2>useNFT</h2>
+        </a>
         <UseNFTHook />
+        <a href="https://portal.thirdweb.com/react/react.useownednfts" target="_blank">
         <h2>useOwnedNFTs</h2>
+        </a>
         <UseOwnedNFTs />
+        <a href="https://portal.thirdweb.com/react/react.uselazymint" target="_blank">
         <h2>useLazyMint</h2>
+        </a>
         <UseLazyMint />
+        <a href="https://portal.thirdweb.com/react/react.useclaimnft" target="_blank">
         <h2>useClaimNFT</h2>
+        </a>
         <UseClaimNFT />
+        <a href="https://portal.thirdweb.com/react/react.useunclaimednfts" target="_blank">
         <h2>useUnclaimedNFTs</h2>
+        </a>
         <UseUnclaimedNFTs />
         {/* <h2>useTotalCirculatingSupply</h2>
         <UseTotalCirculatingSupply />
         <h2>useTotalCount</h2>
         <UseTotalCount /> */}
+        <a href="https://portal.thirdweb.com/react/react.usetransfernft" target="_blank">
         <h2>useTransferNFT</h2>
+        </a>
         <UseTransferNFT />
+        <a href="https://portal.thirdweb.com/react/react.useairdropnft" target="_blank">
         <h2>useAirdrop</h2>
+        </a>
         <UseAirdropHook />
+        <a href="https://portal.thirdweb.com/react/react.useburnnft" target="_blank">
         <h2>useBurnNFT</h2>
+        </a>
         <UseBurnNFT />
         {/* <h2>useMintNFTSupply</h2>
         <UseMintNFTSupply /> */}
+        <a href="https://portal.thirdweb.com/react/react.usenftbalance" target="_blank">
         <h2>useNFTBalance</h2>
+        </a>
         <UseNFTBalance />
       </main>
     </div>
